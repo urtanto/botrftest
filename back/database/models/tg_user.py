964172157301ba93dc0d66a0e642ab69b3bd2cbb
urtanto <vladanikanov19@gmail.com>
2025-03-15 15:04:01 +0300

    setup db and bot

--- back/database/models/tg_user.py
+++ back/database/models/tg_user.py
@@ -1,11 +1,11 @@
 import uuid
 from tortoise import models
-from tortoise.fields import UUIDField, TextField, DateField
+from tortoise.fields import UUIDField, TextField, DateField, CharField
 
 
 class TgUser(models.Model):
     id = UUIDField(pk=True, default=uuid.uuid4)
-    tg_id = TextField(unique=True)
+    tg_id = CharField(max_length=30, unique=True)
     first_name = TextField(null=True)
     last_name = TextField(null=True)
     username = TextField()
