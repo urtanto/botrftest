f3d6b55c833ec843c4fc99a3b6749c1b2bcd3d7e
urtanto <vladanikanov19@gmail.com>
2025-03-16 16:35:49 +0300

    test redirect

--- front/src/components/ViewCard.vue
+++ front/src/components/ViewCard.vue
@@ -94,7 +94,7 @@ setInterval(() => {
 
 const share = () => {
   if (navigator.share) {
-    const link = `https://t.me/robita_task_bot/TestBirtdayMiniApp?user=${userUuid}`
+    const link = `https://t.me/robita_task_bot/TestBirtdayMiniApp?start=${userUuid}`
     navigator.share({
       title: 'Моя карточка',
       url: link,
