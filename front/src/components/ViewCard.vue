60d8f4fd3d3ae4391cb35129ab769a97b33aa804
urtanto <vladanikanov19@gmail.com>
2025-03-16 20:14:16 +0300

    fix buttons

--- front/src/components/ViewCard.vue
+++ front/src/components/ViewCard.vue
@@ -41,14 +41,16 @@
       </tr>
     </table>
 
-    <div v-if="!mycard()" class="flex gap-4 items-center">
+    <div class="flex gap-4 items-center">
       <div class="flex gap-2 items-center">
         <button
+            v-if="mycard()"
             @click="share"
             class="px-6 py-3 bg-white text-purple-600 font-bold rounded shadow hover:bg-purple-100 transition">
           Поделиться
         </button>
         <button
+            v-if="mycard()"
             @click="copyLink"
             class="p-3 bg-white text-purple-600 rounded hover:bg-gray-100 transition">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -56,14 +58,13 @@
                 d="M16 1H4c-1.11 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h11c1.11 0 2-.9 2-2V7c0-1.1-.89-2-2-2zm0 16H8V7h11v14z"/>
           </svg>
         </button>
+        <button
+            v-if="!mycard()"
+            @click="create_my"
+            class="px-6 py-3 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-bold rounded shadow hover:from-purple-600 hover:to-blue-600 transition">
+          Сгенерировать свою карточку
+        </button>
       </div>
-
-      <button
-          v-if="mycard()"
-          @click="create_my"
-          class="px-6 py-3 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-bold rounded shadow hover:from-purple-600 hover:to-blue-600 transition">
-        Сгенерировать свою карточку
-      </button>
     </div>
   </div>
 </template>
