208c5818485e1e50a4bb3ab9581656e4a6489fc3
urtanto <vladanikanov19@gmail.com>
2025-03-16 19:20:22 +0300

    test redirect

--- front/src/components/ViewCard.vue
+++ front/src/components/ViewCard.vue
@@ -34,15 +34,15 @@
     </div>
 
     <button
-        v-if="mycard"
+        v-if="mycard()"
         @click="share"
         class="px-6 py-3 bg-white text-purple-600 font-bold rounded shadow hover:bg-purple-100 transition mb-4">
       Поделиться
     </button>
 
     <button
-        v-if="!mycard"
-        @click="share"
+        v-if="!mycard()"
+        @click=""
         class="px-6 py-3 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-bold rounded shadow hover:from-purple-600 hover:to-blue-600 transition">
       Сгенерировать свою карточку
     </button>
