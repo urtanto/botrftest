e2dac6253b008a0d5f1df09237621142b9126c80
urtanto <vladanikanov19@gmail.com>
2025-03-16 05:58:52 +0300

    testing redirect

--- front/src/components/CreateCard.vue
+++ front/src/components/CreateCard.vue
@@ -88,7 +88,7 @@ async function sendUserData() {
     })
 
     const responseData = await response.json()
-    // router.push(`/user/${responseData.id}`)
+    router.push(`/user/${responseData.id}`)
   } catch (error) {
     alert(error);
   }
