95cd71f6b4b6be196e87646414c1f2ddb1a0a262
urtanto <vladanikanov19@gmail.com>
2025-03-16 05:03:02 +0300

    web server

--- back/main.py
+++ back/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Request
 from database import init_db
 
 app = FastAPI()
@@ -8,6 +10,12 @@ app = FastAPI()
 async def on_startup():
     await init_db()
 
-@app.post("/")
-async def root():
+
+@app.post("/create_card")
+async def root(request: Request):
+    body = await request.body()
+    body_json = json.loads(body.decode("utf-8"))
+
+    print(json.dumps(body_json, indent=2))
+
     return {"message": "Hello World"}
