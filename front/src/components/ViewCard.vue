ff5287d47f739df99c35334752b787fed819c29c
urtanto <vladanikanov19@gmail.com>
2025-03-16 20:03:37 +0300

    fixes

--- front/src/components/ViewCard.vue
+++ front/src/components/ViewCard.vue
@@ -41,7 +41,7 @@
       </tr>
     </table>
 
-    <div v-if="!mycard()" class="flex gap-4 items-center">
+    <div v-if="mycard()" class="flex gap-4 items-center">
       <div class="flex gap-2 items-center">
         <button
             @click="share"
@@ -59,7 +59,7 @@
       </div>
 
       <button
-          v-if="mycard()"
+          v-if="!mycard()"
           @click="create_my"
           class="px-6 py-3 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-bold rounded shadow hover:from-purple-600 hover:to-blue-600 transition">
         Сгенерировать свою карточку
